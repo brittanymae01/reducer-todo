0205ece9cc4ec6c64bccc1fa47587a87812b93cd
brittanymae01 <brittanymae01>
2019-12-10 13:48:30 -0800

    setting up components

---
+++ reducer-todo/src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoForm = () => {
+    return (
+        <div>
+            <input type='text' placeholder='Enter Task' />
+        </div>
+    )
+}
+
+export default TodoForm;
---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,23 @@
+export const initialState = {
+    todos: [{
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    }],
+};
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            const newTodo = {
+                item: action.payload,
+                completed: false,
+                id: new Date()
+            }
+            return {
+                todos: [...state, newTodo]
+            }
+        default:
+            return state;
+    }
+}
