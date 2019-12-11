32733973f8b9dbf96fadbe37df57b3dc79e7ad5e
brittanymae01 <brittanymae01>
2019-12-11 12:06:33 -0800

    you know

--- reducer-todo/src/components/Todo.js
+++ reducer-todo/src/components/Todo.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 
-const Todo = props => {
+const Todo = (props) => {
     return (
-        <div className='list-item' onClick={() => props.completed(props.todo.id)} >
-            <p style={{ textDecoration: props.todo.completed ? 'line-through' : 'none' }}>{props.todo.item}</p>
+        <div className='list-item'
+            // onClick={() => props.completed(props.todo.id)} >
+            onClick={() => props.dispatch({ type: 'TOGGLE_COMPLETE', payload: props.todo.id })} >
+            <p style={{ textDecoration: props.todo.completed ? 'line-through' : 'none' }}> {props.todo.item}</p >
         </div >
     )
 }
--- reducer-todo/src/components/TodoList.js
+++ reducer-todo/src/components/TodoList.js
@@ -6,15 +6,15 @@ import { reducer, initialState } from '../reducers/reducer'
 const TodoList = () => {
     const [taskState, dispatch] = useReducer(reducer, initialState)
 
-    const completed = id => {
-        dispatch({ type: 'TOGGLE_COMPLETE', payload: id })
-    }
+    // const completed = id => {
+    //     dispatch({ type: 'TOGGLE_COMPLETE', payload: id })
+    // }
 
     return (
         <div>
             <TodoForm dispatch={dispatch} />
             <div className='list'> {taskState.todos.map(todo => (
-                <Todo key={todo.id} todo={todo} dispatch={dispatch} completed={completed} />
+                <Todo key={todo.id} todo={todo} dispatch={dispatch} />
             ))}
             </div>
             <button className='clear-button' onClick={() => dispatch({ type: 'CLEAR_COMPLETE' })}>Clear Completed</button>
